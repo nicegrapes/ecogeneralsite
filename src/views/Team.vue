aedd0e53a47cf7a1b0f42f8cd9c445c515711ad2
Vrde Club <>
2020-03-02 21:09:25 -0600

    added team badges

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,20 +1,20 @@
 <template>
 <section
   id="projects"
-  class="mb-n5 py-5"
+  class="mb-n5"
 >
-<heading>Meet Our Team</heading>
+<heading>Meet the Team</heading>
 <v-row
-  class="mb-8 mt-8 py-5"
+  class="ma-8 d-flex flex-wrap"
 >
   <v-card
     class="mx-auto mb-8"
-    max-width="180"
+    max-width="200"
     tile
   >
     <v-img
-      src="/static/martinBackground.png"
-      width="200"
+      src="/static/tomBackground.png"
+      width="270"
     >
       <v-row
         align="center"
@@ -28,7 +28,95 @@
           <v-avatar
             class="profile"
             color="grey"
-            size="200"
+            size="220"
+            tile
+          >
+            <v-img src="/static/profile.jpg" />
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="black"
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                Tom Bridge
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                CEO
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+    <v-card
+    class="mx-auto mb-8"
+    max-width="200"
+    tile
+  >
+    <v-img
+      src="/static/luisBackground.png"
+      width="270"
+    >
+      <v-row
+        align="center"
+        class="fill-height"
+      >
+        <v-col
+          align-self="center"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="220"
+            tile
+          >
+            <v-img src="/static/luisProfile.jpg" />
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="black"
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                Luis Bridge
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Manager
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+    <v-card
+    class="mx-auto mb-8"
+    max-width="200"
+    tile
+  >
+    <v-img
+      src="/static/martinBackground.png"
+      width="270"
+    >
+      <v-row
+        align="center"
+        class="fill-height"
+      >
+        <v-col
+          align-self="center"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="220"
             tile
           >
             <v-img src="/static/martinProfile.png" />
@@ -43,7 +131,97 @@
                 Martin Janes
               </v-list-item-title>
               <v-list-item-subtitle>
-                Marketing and Sales
+                Network Engineer
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+    <v-card
+    class="mx-auto mb-8"
+    max-width="200"
+    tile
+    dark
+  >
+    <v-img
+      src="/static/martinBackground.jpg"
+    >
+      <v-row
+        align="center"
+        class="fill-height"
+      >
+        <v-col
+          align-self="center"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="220"
+            tile
+          >
+            <v-img src="/static/martinProfile.jpg" />
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="black"
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                Martin Janes
+              </v-list-item-title>
+              <v-list-item-subtitle
+                class="mb-2"
+              >
+                Network Engineer
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+  <v-card
+    class="mx-auto mb-8"
+    max-width="200"
+    tile
+  >
+    <v-img
+      src="/static/martinBackground.png"
+      width="270"
+    >
+      <v-row
+        align="center"
+        class="fill-height"
+      >
+        <v-col
+          align-self="center"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="220"
+            tile
+          >
+            <v-img src="/static/martinProfile.png" />
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="black"
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                Martin Janes
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Network Engineer
               </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
